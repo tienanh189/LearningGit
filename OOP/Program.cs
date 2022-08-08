34aefa34316ceeb669531477b5cd84819fb30fe3
tienanh189 <letienanh1892001@gmail.com>
2022-08-08 09:49:10 +0700

    update

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -128,6 +128,7 @@ namespace OOP
 
         static void Main(string[] args)
         {
+            int s = 508;
             Product p1 = new Laptop();
             Product p2 = new Iphone();
             p1.showPrice();
